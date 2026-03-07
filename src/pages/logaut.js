54376970f3412b5a4c4946827e438ce609f6908d
MurodjonHatamov~ <hatamovmurodjon1@gmail.com>
2026-03-07 18:15:40 +0500

    yangilandi

--- src/pages/logaut.js
+++ src/pages/logaut.js
@@ -9,11 +9,11 @@ export const logaut = (response) => {
   
       // alert("⚠️ Token vaqti tugadi. Qayta login qilishingiz kerak!");
   
-      localStorage.removeItem("isLoggedIn");
-      localStorage.removeItem("userType");
-      localStorage.removeItem("loginTime");
+      // localStorage.removeItem("isLoggedIn");
+      // localStorage.removeItem("userType");
+      // localStorage.removeItem("loginTime");
   
-      window.location.replace("/login");
+      // window.location.replace("/login");
   
       return null;
     }
